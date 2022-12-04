db8787d5fd71c3efb1f001d01601878da8076dd4
Brian Denmark <testingarea479@gmail.com>
2022-12-04 06:34:57 -0800

    init commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("My name is Brian.");
+        System.out.println("I am 31 years old.");
+        System.out.println("My hometown is Joplin, Missouri.");
+    }
+}
